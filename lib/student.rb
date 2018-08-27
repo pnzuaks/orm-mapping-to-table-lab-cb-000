f2cba9d680205b29b055d215c315e62dcce00a2b
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-27 19:00:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,8 @@ def save()
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
 
-def self.create()
-  
+def self.create(name:, grade:)
+  student = Student.new(name, grad)
 end
 
 end
